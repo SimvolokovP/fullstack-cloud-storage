9d58e3e2a5c12736d37c2de106a50dbdfd651823
Simvolokov Pavel <psimvolokov@gmail.com>
2026-08-31 17:57:37 +0300

    add backend folder

---
+++ backend/src/main.ts
@@ -0,0 +1,58 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+  app.use(
+    cookieParser(config.getOrThrow<string>('COOKIES_SECRET') || 'secret'),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Cloud Box Api')
+    .setDescription('Cloud Box Api description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Введите валидный Access Token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
+}
+bootstrap();
