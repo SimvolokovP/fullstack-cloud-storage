b9b1da6c7e89b7502357283dfd3133f0a9f6bb1c
Simvolokov Pavel <psimvolokov@gmail.com>
2026-09-02 10:12:05 +0300

    feat: added recaptcha google config

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -14,6 +14,7 @@ import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { User } from '../user/entities/user.entity';
+import { Recaptcha } from '@nestlab/google-recaptcha';
 
 @ApiTags('Authentication')
 @Controller('auth')
@@ -23,6 +24,7 @@ export class AuthController {
     private readonly configService: ConfigService,
   ) {}
 
+  @Recaptcha()
   @Post('register')
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Регистрация нового пользователя' })
@@ -42,6 +44,7 @@ export class AuthController {
     return this.authService.register(req, dto);
   }
 
+  @Recaptcha()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Авторизация пользователя' })
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,9 +6,20 @@ import { User } from '../user/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
+import { GoogleRecaptchaModule } from '@nestlab/google-recaptcha';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getRecaptchaConfig } from 'src/config/recaptcha.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account, Token, User]), UserModule],
+  imports: [
+    TypeOrmModule.forFeature([Account, Token, User]),
+    UserModule,
+    GoogleRecaptchaModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getRecaptchaConfig,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
